52c08c7a5f217662c43b86fc5c1a31b822b99742
Jibin <jibinnajith94@gmail.com>
2024-04-19 12:54:10 +0530

    initial setup

---
+++ android/app/src/main/kotlin/com/jibin/syncfusion_signaturepad/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jibin.syncfusion_signaturepad
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_signaturepad/signaturepad.dart';
+
+void main() {
+  return runApp(const MyHomePage());
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  final GlobalKey<SfSignaturePadState> signatureGlobalKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
+          title: const Text(
+            "Signature Pad",
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+                padding: const EdgeInsets.all(10),
+                child: Container(
+                    decoration:
+                        BoxDecoration(border: Border.all(color: Colors.grey)),
+                    child: SfSignaturePad(
+                        key: signatureGlobalKey,
+                        backgroundColor: Colors.white,
+                        strokeColor: Colors.black,
+                        minimumStrokeWidth: 1.0,
+                        maximumStrokeWidth: 4.0))),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                TextButton(
+                  onPressed: _handleSaveButtonPressed,
+                  child: const Text('Save As Image'),
+                ),
+                TextButton(
+                  onPressed: _handleClearButtonPressed,
+                  child: const Text('Clear'),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _handleClearButtonPressed() {
+    signatureGlobalKey.currentState!.clear();
+  }
+
+  void _handleSaveButtonPressed() async {
+    final data =
+        await signatureGlobalKey.currentState!.toImage(pixelRatio: 3.0);
+    final bytes = await data.toByteData(format: ui.ImageByteFormat.png);
+    navigate(bytes);
+  }
+
+  void navigate(ByteData? bytes) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (BuildContext context) {
+          return Scaffold(
+            appBar: AppBar(),
+            body: Center(
+              child: Container(
+                color: Colors.grey[300],
+                child: Image.memory(bytes!.buffer.asUint8List()),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
